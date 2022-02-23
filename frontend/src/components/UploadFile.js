c6648837b686cab7fc8ecf6b8f34775480d1929c
aminadaven <aminadaven@gmail.com>
2022-02-23 12:57:59 +0200

    Commented out unused component for ci to pass

--- frontend/src/components/UploadFile.js
+++ frontend/src/components/UploadFile.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
-import UploadResponse from './UploadResponse'
+//import UploadResponse from './UploadResponse'
 
 export default class UploadFile extends Component {
 //  constructor (props) {
