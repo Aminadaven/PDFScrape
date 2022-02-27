fd68183731dc42e92734f81d242a44c732ac4330
aminadaven <aminadaven@gmail.com>
2022-02-27 17:59:19 +0200

    Added refresh ability to front

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import Container from 'react-bootstrap/Container'
 
@@ -6,15 +6,30 @@ import './App.css'
 
 import UploadFile from './components/UploadFile'
 import Files from './components/Files'
+import UploadResponse from './components/UploadResponse'
 
-const App = () => (
-  <Container className='p-3'>
-    <Container className='p-5 mb-4 bg-light rounded-3'>
-      <h1 className='header'>PDF - Scrape</h1>
-      <UploadFile />
-      <Files />
-    </Container>
-  </Container>
-)
+export default class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = { uploadResponse: null }
+  }
 
-export default App
+  updateStatus (response, isError) {
+    this.setState({
+        uploadResponse: <UploadResponse response={response} isError={isError} />
+    })
+  }
+
+  render () {
+    return (
+      <Container className='p-3'>
+        <Container className='p-5 mb-4 bg-light rounded-3'>
+          <h1 className='header'>PDF - Scrape</h1>
+          {/* {this.state.uploadResponse} */}
+          <UploadFile updateStatus={this.updateStatus.bind(this)} />
+          <Files />
+        </Container>
+      </Container>
+    )
+  }
+}
