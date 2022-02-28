b879c3429f9824adc4144c9c648b3a9d4a5f106f
Aminadaven <61430170+Aminadaven@users.noreply.github.com>
2022-02-28 14:46:48 +0200

    Reconstruct UploadResponse

--- frontend/src/components/UploadResponse.js
+++ frontend/src/components/UploadResponse.js
@@ -1,20 +1,18 @@
 import React from 'react'
 import Alert from 'react-bootstrap/Alert'
 
-const UploadResponse = ({ response, isError }) => {
-  if (isError === null) {
-    return <></>
-  } else if (isError === true || isError === undefined) {
+const UploadResponse = ({ response, status, close }) => {
+  if (status === 200) {
     return (
-      <Alert variant='danger' dismissible>
-        <Alert.Heading>Oh snap! You got an error! {isError} </Alert.Heading>
+      <Alert variant='success' onClose={() => close()} dismissible>
+        <Alert.Heading>Success!</Alert.Heading>
         <p>{response}</p>
       </Alert>
     )
   } else {
     return (
-      <Alert variant='success'>
-        <Alert.Heading>Success!</Alert.Heading>
+      <Alert variant='danger' onClose={() => close()} dismissible>
+        <Alert.Heading>Oh snap! You got an error! {isError} </Alert.Heading>
         <p>{response}</p>
       </Alert>
     )
