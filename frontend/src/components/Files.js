d202a83c9b20b0e89dfa228b4a61b3bf5545e5ff
aminadaven <aminadaven@gmail.com>
2022-02-28 20:17:51 +0200

    Fixed all problems Added ErrorBoundary Server-Client 'talking' is now fully working The frontend now shows errors on from the backend on the UI as a closable alert

--- frontend/src/components/Files.js
+++ frontend/src/components/Files.js
@@ -1,8 +1,10 @@
-import React, { Component } from 'react'
+import { Component } from 'react'
 import ListGroup from 'react-bootstrap/ListGroup'
 import FileCard from './FileCard'
 
 export default class Files extends Component {
+  _isMounted = false
+
   constructor (props) {
     super(props)
     this.state = {
@@ -10,17 +12,23 @@ export default class Files extends Component {
     }
   }
 
-  fetchFiles() {
+  fetchFiles () {
     fetch(`http://${process.env.BACKEND_HOST || 'localhost'}:8080/files`)
       .then(response => response.json())
-      .then(files => this.setState({ files }))
+      .then(files => {
+        if (this._isMounted) {
+          this.setState({ files })
+        }
+      })
   }
 
   componentDidMount () {
-    this.interval = setInterval(() => this.fetchFiles(), 500);
+    this._isMounted = true
+    this.interval = setInterval(() => this.fetchFiles(), 500)
   }
-  componentWillUnmount() {
-    clearInterval(this.interval);
+  componentWillUnmount () {
+    clearInterval(this.interval)
+    this._isMounted = false
   }
 
   render () {
