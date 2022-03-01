36e978c13133bf06cb0c3994e1d294f834f390ef
aminadaven <aminadaven@gmail.com>
2022-03-01 08:40:35 +0200

    Reformatted Code

--- frontend/src/components/Files.js
+++ frontend/src/components/Files.js
@@ -1,45 +1,46 @@
-import { Component } from 'react'
+import {Component} from 'react'
 import ListGroup from 'react-bootstrap/ListGroup'
 import FileCard from './FileCard'
 
 export default class Files extends Component {
-  _isMounted = false
+    _isMounted = false
 
-  constructor (props) {
-    super(props)
-    this.state = {
-      files: []
-    }
-  }
-
-  fetchFiles () {
-    fetch(`http://${process.env.BACKEND_HOST || 'localhost'}:8080/files`)
-      .then(response => response.json())
-      .then(files => {
-        if (this._isMounted) {
-          this.setState({ files })
+    constructor(props) {
+        super(props)
+        this.state = {
+            files: []
         }
-      })
-  }
+    }
 
-  componentDidMount () {
-    this._isMounted = true
-    this.interval = setInterval(() => this.fetchFiles(), 500)
-  }
-  componentWillUnmount () {
-    clearInterval(this.interval)
-    this._isMounted = false
-  }
+    fetchFiles() {
+        fetch(`http://${process.env.BACKEND_HOST || 'localhost'}:8080/files`)
+            .then(response => response.json())
+            .then(files => {
+                if (this._isMounted) {
+                    this.setState({files})
+                }
+            })
+    }
 
-  render () {
-    return (
-      <ListGroup variant='flush'>
-        {this.state.files.map(file => (
-          <ListGroup.Item key={file.id}>
-            <FileCard key={file.id} file={file} />
-          </ListGroup.Item>
-        ))}
-      </ListGroup>
-    )
-  }
+    componentDidMount() {
+        this._isMounted = true
+        this.interval = setInterval(() => this.fetchFiles(), 500)
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval)
+        this._isMounted = false
+    }
+
+    render() {
+        return (
+            <ListGroup variant='flush'>
+                {this.state.files.map(file => (
+                    <ListGroup.Item key={file.id}>
+                        <FileCard key={file.id} file={file}/>
+                    </ListGroup.Item>
+                ))}
+            </ListGroup>
+        )
+    }
 }
--- frontend/src/components/UploadResponse.js
+++ frontend/src/components/UploadResponse.js
@@ -1,25 +1,24 @@
 import React from 'react'
 import Alert from 'react-bootstrap/Alert'
 
-function UploadResponse (props) {
-  console.log(props)
-  if (props.status === 200) {
-    return (
-      <Alert variant='success' onClose={props.close} dismissible>
-        <Alert.Heading>Success!</Alert.Heading>
-        <p>{props.response}</p>
-      </Alert>
-    )
-  } else {
-    return (
-      <Alert variant='danger' onClose={props.close} dismissible>
-        <Alert.Heading>
-          There was an error. Code: {props.status}
-        </Alert.Heading>
-        <p>{props.response}</p>
-      </Alert>
-    )
-  }
+function UploadResponse(props) {
+    if (props.status === 200) {
+        return (
+            <Alert variant='success' onClose={props.close} dismissible>
+                <Alert.Heading>Success!</Alert.Heading>
+                <p>{props.response}</p>
+            </Alert>
+        )
+    } else {
+        return (
+            <Alert variant='danger' onClose={props.close} dismissible>
+                <Alert.Heading>
+                    There was an error. Code: {props.status}
+                </Alert.Heading>
+                <p>{props.response}</p>
+            </Alert>
+        )
+    }
 }
 
 export default UploadResponse
