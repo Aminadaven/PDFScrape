a04f5e09f401d51c18f69e53dbd6350b84c2c39b
aminadaven <aminadaven@gmail.com>
2022-03-01 08:42:27 +0200

    Precise and prettier size showing, now using size units KB, MB, GB

--- frontend/src/components/FileCard.js
+++ frontend/src/components/FileCard.js
@@ -2,14 +2,28 @@ import React from 'react'
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
 
-const FileCard = ({ file }) => (
-  <Card>
-    <Card.Header>{file.name}</Card.Header>
-    <Card.Body>
-      <Card.Text>Size: {file.size / 1024} KBs.</Card.Text>
-      <Button href={file.url}>Download Excel File</Button>
-    </Card.Body>
-  </Card>
+const leastExponentOf = (num, base, startValue = 0) => {
+    let exponent = startValue;
+    while (num > base ** exponent) {
+        exponent++;
+    }
+    return exponent;
+}
+
+const sizeInUnits = (size) => {
+    const sizes = ['Bytes', 'KB', 'MB', 'GB'];
+    const exponent = leastExponentOf(size, 1024, 1);
+    return size / (1024 ** exponent) + ' ' + sizes[exponent - 1];
+}
+
+const FileCard = ({file}) => (
+    <Card>
+        <Card.Header>{file.name}</Card.Header>
+        <Card.Body>
+            <Card.Text>Size: {sizeInUnits(file.size)}.</Card.Text>
+            <Button href={file.url}>Download Excel File</Button>
+        </Card.Body>
+    </Card>
 )
 
 export default FileCard
